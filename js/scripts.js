0ae0ed567616db3e63a55612e94c00831a7f0211
cameron jensen and jahan walsh <jahan@>
2017-02-16 09:56:01 -0800

    add images of spider, anteater, and dragon. Delete variable in else function. Change spelling errors in header and update bootstrap link.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(function(){
-  var animal = parseInt(prompt("If you would like to learn about Tarantulas enter 1, if you want to learn about Ant-Eaters enter 2, if you want to learn about Kommodo Dragons enter 3" ));
+  var animal = parseInt(prompt("If you would like to learn about Tarantulas enter 1, if you want to learn about Ant-Eaters enter 2, if you want to learn about Kommodo Dragons enter 3"));
 
   if (animal === 1) {
     $('#spider').show();
@@ -7,10 +7,7 @@ $(function(){
     $('#ant').show();
   } else if (animal === 3) {
     $('#dragon').show();
-  } else (animal !== 1, 2, 3) {
-    alert ("Please refresh page and enter only 1, 2, or 3!");
+  } else {
+    alert("Please refresh page and enter only 1, 2, or 3!");
   }
-
-
-
 });
